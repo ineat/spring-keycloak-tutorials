3cbb0fe538208e895d524fea4c29f64ec071eb1e
Ludovic <ludovic.dussart@ineat-conseil.fr>
2017-11-16 18:20:42 +0100

    chore: init project files from Initializr

---
+++ src/test/java/com/ineat/tutorials/springkeycloaktutorials/SpringKeycloakTutorialsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ineat.tutorials.springkeycloaktutorials;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringKeycloakTutorialsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
