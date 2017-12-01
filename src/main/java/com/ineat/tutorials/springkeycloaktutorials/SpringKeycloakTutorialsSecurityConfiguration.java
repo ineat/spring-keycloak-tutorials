3abe40ddc2fa037381fda28b7c694a1cec6b6569
Ludovic <ludovic.dussart@ineat-conseil.fr>
2017-12-01 18:27:47 +0100

    chore(test): add tests for endpoints matchers using CommonSpringKeycloakTutorialsSecuritAdapter

--- src/main/java/com/ineat/tutorials/springkeycloaktutorials/SpringKeycloakTutorialsSecurityConfiguration.java
+++ src/main/java/com/ineat/tutorials/springkeycloaktutorials/SpringKeycloakTutorialsSecurityConfiguration.java
@@ -1,20 +1,14 @@
 package com.ineat.tutorials.springkeycloaktutorials;
 
-import org.keycloak.adapters.KeycloakConfigResolver;
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
